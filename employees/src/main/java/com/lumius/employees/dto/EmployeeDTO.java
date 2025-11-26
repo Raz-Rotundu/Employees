6cdb132bc3b04dada29bdd7d9b573d9f48758631
Razvan Rotundu <raz.rotundu@gmail.com>
2025-11-26 14:36:37 -0500

    Define EmployeeService interface
    Define basic CRUD operations

--- employees/src/main/java/com/lumius/employees/dto/EmployeeDTO.java
+++ employees/src/main/java/com/lumius/employees/dto/EmployeeDTO.java
@@ -9,16 +9,37 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
+/**
+ * @author Razvan
+ * EmployeeDTO represents an employee in the Employees table
+ * All fields are required except for organizationNode and organizationLevel
+ * LocalDate and LocalDateTime fields are specifically parsed according to pattern
+ */
 @Builder
+@Data
+@EqualsAndHashCode
+@ToString
 public class EmployeeDTO {
 
+	/**
+	 * The ID of the employee within the company, uniquely generated
+	 */
 	@NotNull(message = "Business Entity ID cannot be null")
 	UUID businessEntityID;
 	
+	/**
+	 * The Country ID of the employee representing what country they work in
+	 */
 	@NotEmpty(message = "National ID cannot be empty")
 	String nationalIDNumber;
 	
+	/**
+	 * The login ID/username for the user in the company portal
+	 */
 	@NotEmpty(message = "loginId cannot be empty")
 	String loginID;
 	
@@ -26,38 +47,70 @@ public class EmployeeDTO {
 	String organizationNode;
 	String organizationLevel;
 	
+	/**
+	 * Employee job title
+	 */
 	@NotEmpty(message = "Job title cannot be empty")
 	String jobTitle;
 	
+	/**
+	 * Employee birth date, formatted to yyyy-MM-dd
+	 */
 	@JsonFormat(pattern = "yyyy-MM-dd")
 	@NotNull(message = "Birthdate cannot be null")
 	LocalDate birthDate;
 	
+	/**
+	 * Employee marital status
+	 */
 	@NotEmpty(message = "Marital status cannot be empty")
 	String maritalStatus;
 	
+	/**
+	 * Employee gender
+	 */
 	@NotEmpty(message = "Gender cannot be empty")
 	String gender;
 	
+	/**
+	 * Date on which the employee was hired
+	 * Formatted to yyyy-MM-dd
+	 */
 	@JsonFormat(pattern = "yyyy-MM-dd")
 	@NotNull(message = "Hiring date Cannot be empty")
 	LocalDate hireDate;
 	
+	/**
+	 * Indicates if the employee is salaried or not (T or F)
+	 */
 	@NotEmpty(message = "Salaried flag cannot be empty")
 	String salariedFlag;
 	
+	/**
+	 * Number of vacation hours the employee is entitled to
+	 */
 	@NotNull(message = "Vacation hours cannot be empty")
 	Integer vacationHours;
 	
+	/**
+	 * Number of sick leave hours an employee is entitled to 
+	 */
 	@NotNull(message = "Sick leave hours cannot be empty")
 	Integer sickLeaveHours;
 	
+	/**
+	 * Indicates if this person is currently employed at the company (T or F)
+	 */
 	@NotEmpty(message = "Current flag cannot be empty")
 	String currentFlag;
 	
+
 	@NotNull(message = "Row GUID cannot be empty")
 	UUID rowGuid;
 	
+	/**
+	 * Last time this employee record was modified
+	 */
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSSS")
 	@NotNull(message = "Modified date cannot be empty")
 	LocalDateTime modifiedDate;
---
+++ employees/src/main/java/com/lumius/employees/service/EmployeeService.java
@@ -0,0 +1,65 @@
+package com.lumius.employees.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.lumius.employees.dto.EmployeeDTO;
+
+/**
+ * @author Razvan 
+ * Interface describing the operations which can be done on the Employees table.
+ * Supported operations are the basic CRUD operations
+ */
+public interface EmployeeService {
+
+	// Create
+	/**
+	 * Saves a new employee to the repository
+	 * @param newEmployee the new employee to be saved
+	 * @return Optional containing newly created employee
+	 */
+	Optional<EmployeeDTO> saveEmployee(EmployeeDTO newEmployee);
+	
+	// Read
+	/**
+	 * Retrieves existing employee from repository based on ID
+	 * @param id the ID of the employee to be retrieved
+	 * @return Optional containing employee if found
+	 */
+	Optional<EmployeeDTO> getEmployeeByID(UUID id);
+	
+	/**
+	 * Retrieves all employees present in the repository
+	 * @return A list of all present Employees in the repository
+	 */
+	List<EmployeeDTO> getAllEmployees();
+	
+	// Update
+	/**
+	 * Update an existing employee, fully replacing the content
+	 * @param id ID of an existing employee
+	 * @param updatedEmployee Employee with which to replace the existing entry
+	 * @return Optional containing updated employee it it exists
+	 */
+	Optional<EmployeeDTO> updateEmployee(UUID id, EmployeeDTO updatedEmployee);
+	
+	/**
+	 * Update specific fields of an existing employee
+	 * @param id ID of an existing employee
+	 * @param partialEmployee A partially constructed employee, with only fields to be updated having values
+	 * @return
+	 */
+	Optional<EmployeeDTO> updateEmployeeFields(UUID id, EmployeeDTO partialEmployee);
+	
+	// Delete
+	/**
+	 * Delete an existing employee from the repository
+	 * @param id ID of existing employee
+	 * @return Empty Optional 
+	 */
+	Optional<Void> deleteEmployeeById(UUID id);
+
+	
+	
+}
