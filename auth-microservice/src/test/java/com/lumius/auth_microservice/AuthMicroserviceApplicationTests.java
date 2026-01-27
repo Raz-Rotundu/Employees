d1f3d3fbd5a3754d6a3dfa3dfcd8dbd983fdb1d2
Razvan Rotundu <raz.rotundu@gmail.com>
2026-01-27 12:24:57 -0500

    Auth-microservice initial commit

---
+++ auth-microservice/src/test/java/com/lumius/auth_microservice/AuthMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumius.auth_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
